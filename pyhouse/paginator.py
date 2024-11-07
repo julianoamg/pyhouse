a1d13316e809d22b54db50de858b873839de4534
Your Name <you@example.com>
2024-11-07 12:39:14 -0300

    Add HousePaginator class

---
+++ pyhouse/paginator.py
@@ -0,0 +1,27 @@
+import math
+
+
+class HousePaginator:
+    page = 0
+    has_next = False
+    has_previous = False
+
+    def __init__(self, query, page, per_page):
+        self.query = query
+        self.page = page
+        self.per_page = per_page
+        self.count = self.query.count()
+        self.query = self.query.max(self.per_page).offset((self.page - 1) * self.per_page)
+        self.total_pages = int(math.ceil(self.count / self.per_page))
+        self.has_next = page < self.total_pages
+        self.has_previous = page > 1
+
+    def as_dict(self):
+        return {
+            "current_page": self.page,
+            "total_pages": self.total_pages,
+            "has_previous": self.has_previous,
+            "has_next": self.has_next,
+            "total_items": self.count,
+            "per_page": self.per_page
+        },
