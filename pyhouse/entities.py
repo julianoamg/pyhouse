368783147a7739815d9552961428d2060db67a1c
Your Name <you@example.com>
2024-11-07 21:51:24 -0300

    Fix wrong isinstance param order in entities.py

--- pyhouse/entities.py
+++ pyhouse/entities.py
@@ -53,7 +53,7 @@ class Entity(metaclass=Meta):
 
             if casts:
                 for cast in casts:
-                    if isinstance(cast[0], v):
+                    if isinstance(v, cast[0]):
                         v = cast[1](v)
                         break
 
